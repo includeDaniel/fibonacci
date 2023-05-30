bdf0eb11c7e88b382d67ae89b87c7b8a6205ece6
Daniel Silva <danielzcomputacao@gmail.com>
2023-05-30 14:57:11 -0300

    Adding memoization to gain speed

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const fibonacci = (n, prev = new Map()) => {
     if (n < 0) throw new Error("only accepts positive values")
-    if (n == 0) return n
-    if (n <= 2 && n > 0) return 1
-    return (fibonacci(n - 1) + fibonacci(n - 2))
-
+    if (prev.has(n)) return prev.get(n)
+    if (n == 0) {
+        prev.set(n, 0)
+        return 0
+    }
+    if (n <= 2 && n > 0) {
+        prev.set(n, 1)
+        return 1
+    }
+    next = (fibonacci(n - 1, prev) + fibonacci(n - 2, prev))
+    prev.set(n, next)
+    return next
 }
-
 
 module.exports = fibonacci;
