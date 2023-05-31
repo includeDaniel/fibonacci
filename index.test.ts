9078e5efcdf52c8f71ebd5890e63b9a6071a58e8
Daniel Silva <danielzcomputacao@gmail.com>
2023-05-31 17:08:10 -0300

    adding prettier and coverage

--- index.test.ts
+++ index.test.ts
@@ -1,34 +1,39 @@
-import fibonacci from ".";
+import fibonacci from '.'
 
 describe('fibonacci', () => {
-    test('the 0 term should return 0', () => {
-        expect(fibonacci(0)).toBe(0);
-    });
-    test('the 1 term should return 1', () => {
-        expect(fibonacci(1)).toBe(1);
-    });
-    test('the 2 term should return 1', () => {
-        expect(fibonacci(2)).toBe(1);
-    });
-    test('the 3 term should return 2', () => {
-        expect(fibonacci(3)).toBe(2);
-    });
-    test('the 4 term should return 3', () => {
-        expect(fibonacci(4)).toBe(3);
-    });
-    test('the 10 term should return 55', () => {
-        expect(fibonacci(10)).toBe(55);
-    });
-    test('the 13 term should return 233', () => {
-        expect(fibonacci(13)).toBe(233);
-    });
-    test('the 18 term should return 2584', () => {
-        expect(fibonacci(18)).toBe(2584);
-    });
-    test('the 20 term should return 6765', () => {
-        expect(fibonacci(20)).toBe(6765);
-    });
-    test('the 23 term should return 28657', () => {
-        expect(fibonacci(23)).toBe(28657);
-    });
-});
+  test('the 0 term should return 0', () => {
+    expect(fibonacci(0)).toBe(0)
+  })
+  test('the 1 term should return 1', () => {
+    expect(fibonacci(1)).toBe(1)
+  })
+  test('the 2 term should return 1', () => {
+    expect(fibonacci(2)).toBe(1)
+  })
+  test('the 3 term should return 2', () => {
+    expect(fibonacci(3)).toBe(2)
+  })
+  test('the 4 term should return 3', () => {
+    expect(fibonacci(4)).toBe(3)
+  })
+  test('the 10 term should return 55', () => {
+    expect(fibonacci(10)).toBe(55)
+  })
+  test('the 13 term should return 233', () => {
+    expect(fibonacci(13)).toBe(233)
+  })
+  test('the 18 term should return 2584', () => {
+    expect(fibonacci(18)).toBe(2584)
+  })
+  test('the 20 term should return 6765', () => {
+    expect(fibonacci(20)).toBe(6765)
+  })
+  test('the 23 term should return 28657', () => {
+    expect(fibonacci(23)).toBe(28657)
+  })
+  test('the -1 term should return a Error', () => {
+    expect(() => fibonacci(-1)).toThrow(
+      new Error('only accepts positive values')
+    )
+  })
+})
--- index.ts
+++ index.ts
@@ -1,16 +1,20 @@
-const fibonacci = (n: number, prev = new Map<number, number>()) : number => {
-    if (n < 0) throw new Error("only accepts positive values")
-    if (prev.has(n)) return prev.get(n)!
-    if (n == 0) {
-        prev.set(n, 0)
-        return 0
-    }
-    if (n <= 2 && n > 0) {
-        prev.set(n, 1)
-        return 1
-    }
-    const next = (fibonacci(n - 1, prev) + fibonacci(n - 2, prev))
-    prev.set(n, next)
-    return next
+const fibonacci = (
+  n: number,
+  prev = new Map<number, number>(),
+  error: string = 'only accepts positive values'
+): number => {
+  if (n < 0) throw new Error(error)
+  if (prev.has(n)) return prev.get(n)!
+  if (n == 0) {
+    prev.set(n, 0)
+    return 0
+  }
+  if (n <= 2 && n > 0) {
+    prev.set(n, 1)
+    return 1
+  }
+  const next = fibonacci(n - 1, prev, error) + fibonacci(n - 2, prev, error)
+  prev.set(n, next)
+  return next
 }
-export default fibonacci;
+export default fibonacci
