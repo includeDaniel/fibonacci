cc6e658528c8c469545af3f5a75d4572cbf77ba3
Daniel Silva <danielzcomputacao@gmail.com>
2023-05-31 17:09:54 -0300

    adding editorconfig

--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
 const fibonacci = (
-  n: number,
-  prev = new Map<number, number>(),
-  error: string = 'only accepts positive values'
+    n: number,
+    prev = new Map<number, number>(),
+    error: string = 'only accepts positive values'
 ): number => {
-  if (n < 0) throw new Error(error)
-  if (prev.has(n)) return prev.get(n)!
-  if (n == 0) {
-    prev.set(n, 0)
-    return 0
-  }
-  if (n <= 2 && n > 0) {
-    prev.set(n, 1)
-    return 1
-  }
-  const next = fibonacci(n - 1, prev, error) + fibonacci(n - 2, prev, error)
-  prev.set(n, next)
-  return next
+    if (n < 0) throw new Error(error)
+    if (prev.has(n)) return prev.get(n)!
+    if (n == 0) {
+        prev.set(n, 0)
+        return 0
+    }
+    if (n <= 2 && n > 0) {
+        prev.set(n, 1)
+        return 1
+    }
+    const next = fibonacci(n - 1, prev, error) + fibonacci(n - 2, prev, error)
+    prev.set(n, next)
+    return next
 }
 export default fibonacci
